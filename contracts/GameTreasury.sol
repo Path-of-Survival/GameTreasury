4a17a203975f0864db5afb34923db4b6030603b5
krumbgf <krumbgf@gmail.com>
2022-04-21 19:25:15 +0300

    add mint NFT

--- contracts/GameTreasury.sol
+++ contracts/GameTreasury.sol
@@ -1,23 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
+import "./access/AdminPrivileges.sol";
 import "./cryptography/EIP712.sol";
 
-contract GameTreasury is Ownable, EIP712, ERC721Holder
+interface IPoSNFT
+{
+    function mintByAdmin(address to, uint token_id) external;
+    function mintByAdmin(address to, uint token_id, uint[] memory metadata) external;
+}
+
+contract GameTreasury is AdminPrivileges, EIP712, ERC721Holder
 {
     using SafeERC20 for IERC20;
-    
-    mapping (address => bool) public admins;
 
     mapping(uint=>bool) private withdraw_ids;
     bytes32 constant private CLAIM_ERC20_TYPE_HASH = 0xea348efde5622ad29c55ef03a95167ea9fb6d2512dff03cffda98339c786bf56;
     bytes32 constant private CLAIM_ERC721_TYPE_HASH = 0xc8066efde1cf94c6fbfd2d8a44d68c0505a867bc075e2a1019b483e12ae3228a;
+    bytes32 constant private MINT_TYPE_HASH = 0xc792717ac7924b7fba148e24b5244a71f5f6ea777d9fbd8c6fda825ba8f187ab;
+    bytes32 constant private MINT_METADATA_TYPE_HASH = 0xcc6fb495d18c2f4149eb2409d4c09954d40ad7f329ac3cbfaac1e30fddc0cfa7;
 
     event TokensDeposited(address _from, address _contract_address, uint _amount);
     event TokensWithdrawn(address _to, address _contract_address, uint _amount, uint _withdrawId);
@@ -42,10 +48,33 @@ contract GameTreasury is Ownable, EIP712, ERC721Holder
         emit NFTDeposited(_msgSender(), erc721_address, tokenId);
     }
 
+    function mint(address posnft_address, uint tokenId, uint withdrawId, bytes memory signature) external
+    {
+        bytes32 data_hash = keccak256(abi.encode(MINT_TYPE_HASH, _msgSender(), posnft_address, tokenId, withdrawId));
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
+        IPoSNFT posnft = IPoSNFT(posnft_address);
+        posnft.mintByAdmin(_msgSender(), tokenId);
+        withdraw_ids[withdrawId] = true;
+        emit NFTWithdrawn(_msgSender(), posnft_address, tokenId, withdrawId);
+    }
+
+    function mintWithMetadata(address posnft_address, uint tokenId, uint256[] memory metadata, uint withdrawId, bytes memory signature) external
+    {
+        bytes32 data_hash = keccak256(abi.encode(MINT_METADATA_TYPE_HASH, _msgSender(), posnft_address, tokenId, keccak256(abi.encodePacked(metadata)), withdrawId));
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
+        IPoSNFT posnft = IPoSNFT(posnft_address);
+        posnft.mintByAdmin(_msgSender(), tokenId, metadata);
+        withdraw_ids[withdrawId] = true;
+        emit NFTWithdrawn(_msgSender(), posnft_address, tokenId, withdrawId);
+    }
+
     function claimERC20(address erc20_address, uint amount, uint withdrawId, bytes memory signature) external
     {
         bytes32 data_hash = keccak256(abi.encode(CLAIM_ERC20_TYPE_HASH, _msgSender(), erc20_address, amount, withdrawId));
-        require(withdraw_ids[withdrawId] == false && admins[EIP712.verify(data_hash, signature)] == true, "invalid signature");
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
         IERC20 token = IERC20(erc20_address);
         token.transfer(_msgSender(), amount);
         withdraw_ids[withdrawId] = true;
@@ -55,9 +84,10 @@ contract GameTreasury is Ownable, EIP712, ERC721Holder
     function claimERC721(address erc721_address, uint tokenId, uint withdrawId, bytes memory signature) external
     {
         bytes32 data_hash = keccak256(abi.encode(CLAIM_ERC721_TYPE_HASH, _msgSender(), erc721_address, tokenId, withdrawId));
-        require(withdraw_ids[withdrawId] == false && admins[EIP712.verify(data_hash, signature)] == true, "invalid signature");
-        IERC721 token = IERC721(erc721_address);
-        token.safeTransferFrom(address(this), _msgSender(), tokenId);
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
+        IERC721 nft = IERC721(erc721_address);
+        nft.safeTransferFrom(address(this), _msgSender(), tokenId);
         withdraw_ids[withdrawId] = true;
         emit NFTWithdrawn(_msgSender(), erc721_address, tokenId, withdrawId);
     }
@@ -65,7 +95,8 @@ contract GameTreasury is Ownable, EIP712, ERC721Holder
     function cancelWithdrawERC20(address erc20_address, uint amount, uint withdrawId, bytes memory signature) external
     {
         bytes32 data_hash = keccak256(abi.encode(CLAIM_ERC20_TYPE_HASH, _msgSender(), erc20_address, amount, withdrawId));
-        require(withdraw_ids[withdrawId] == false && admins[EIP712.verify(data_hash, signature)] == true, "invalid signature");
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
         withdraw_ids[withdrawId] = true;
         emit WithdrawRequestCancelled(withdrawId);
     }
@@ -73,18 +104,27 @@ contract GameTreasury is Ownable, EIP712, ERC721Holder
     function cancelWithdrawERC721(address erc721_address, uint tokenId, uint withdrawId, bytes memory signature) external
     {
         bytes32 data_hash = keccak256(abi.encode(CLAIM_ERC721_TYPE_HASH, _msgSender(), erc721_address, tokenId, withdrawId));
-        require(withdraw_ids[withdrawId] == false && admins[EIP712.verify(data_hash, signature)] == true, "invalid signature");
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
         withdraw_ids[withdrawId] = true;
         emit WithdrawRequestCancelled(withdrawId);
     }
 
-    function addAdmin(address admin) external onlyOwner
+    function cancelMint(address posnft_address, uint tokenId, uint withdrawId, bytes memory signature) external
     {
-        admins[admin] = true;
+        bytes32 data_hash = keccak256(abi.encode(MINT_TYPE_HASH, _msgSender(), posnft_address, tokenId, withdrawId));
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
+        withdraw_ids[withdrawId] = true;
+        emit WithdrawRequestCancelled(withdrawId);
     }
 
-    function removeAdmin(address admin) external onlyOwner
+    function cancelMintWithMetadata(address posnft_address, uint tokenId, uint256[] memory metadata, uint withdrawId, bytes memory signature) external
     {
-        admins[admin] = false;
+        bytes32 data_hash = keccak256(abi.encode(MINT_METADATA_TYPE_HASH, _msgSender(), posnft_address, tokenId, keccak256(abi.encodePacked(metadata)), withdrawId));
+        require(withdraw_ids[withdrawId] == false, "invalid withdrawId");
+        require(isAdmin(EIP712.verify(data_hash, signature)) == true, "invalid signature");
+        withdraw_ids[withdrawId] = true;
+        emit WithdrawRequestCancelled(withdrawId);
     }
 }
--- scripts/sign_gametreasury.js
+++ scripts/sign_gametreasury.js
@@ -4,30 +4,58 @@
         console.log('Running GameTreasury sign test...')
         const account = (await web3.eth.getAccounts())[0];
 
-        console.log("address", account)
+//        console.log("address", account)
 
         const name = "GameTreasury";
         const version = "1.0";
         const chain_id = await web3.eth.getChainId();
-        const contract_address = "0x00193cc2d32a27d7d6f99172f346ed58bc9b10a9";
+        const contract_address = "0x938EE51495B6285FD0A684D40F7c5e47CC818530";
         const salt = "0x0caaadd5fd4d8a10894f330ae16c94ab15949942ce2610697319e90402db033f";
 
         const token_address = "0xE6c471121b974dce211b65eF41E7E17D53Be879d";
         const nft_address = "0x6848d4fb6603a0254ef876070b078067a5f0e6c0";
-        
-        const domain_separator = domainSeparator(name, version, chain_id, contract_address, salt); 
-        
-//        console.log(domain_separator);
-        var typed_data = toTypedDataHash(domain_separator, claimERC721Hash(nft_address, "0xF1F6720d4515934328896D37D356627522D97B49", 1, 2));
-//        var typed_data = toTypedDataHash(domain_separator, claimERC20Hash(token_address, "0xF1F6720d4515934328896D37D356627522D97B49", 10, 1));
-        console.log(typed_data);
 
-        console.log( await web3.eth.sign(typed_data, account))
+        const receiver_address = "0xF1F6720d4515934328896D37D356627522D97B49";
+        
+        const domain_separator = domainSeparator(name, version, chain_id, contract_address, salt);      
+//        console.log("domain_separator", domain_separator);
+//        var typed_data = toTypedDataHash(domain_separator, claimERC721Hash(nft_address, receiver_address, 1, 2));
+//        var typed_data = toTypedDataHash(domain_separator, claimERC20Hash(token_address, receiver_address, 10, 1));
+//        var typed_data = toTypedDataHash(domain_separator, mintHash("0x69773180DD8e165D6c93d659Ec8b87ACC4391Cf2", receiver_address, 2, 1));
+        var typed_data = toTypedDataHash(domain_separator, mintWithMetadataHash("0x6D2D9F3a10DecD027BD5862d469207201274cA09", receiver_address, 10, [1,2,3], 3));
+        console.log("typed_data", typed_data);
+
+        console.log("signature", await web3.eth.sign(typed_data, account))
     
     } catch (e) {
         console.log(e.message)
     }
 })()
+
+function mintWithMetadataHash(posnft_address, to, tokenId, metadata, withdrawId)
+{
+//    console.log("mint metadata nft type hash", web3.utils.keccak256(web3.utils.utf8ToHex("mintWithMetadata(address to,address posnft_address,uint256 tokenId,uint256[] metadata,uint256 withdrawId)")))
+    return web3.utils.keccak256(web3.eth.abi.encodeParameters(["bytes32", "address", "address", "uint256", "bytes32", "uint256"], [
+            web3.utils.keccak256(web3.utils.utf8ToHex("mintWithMetadata(address to,address posnft_address,uint256 tokenId,uint256[] metadata,uint256 withdrawId)")),
+            to,
+            posnft_address,
+            tokenId,
+            web3.utils.keccak256(web3.eth.abi.encodeParameter(`uint256[${metadata.length}]`, metadata)),
+            withdrawId
+        ]));
+}
+
+function mintHash(posnft_address, to, tokenId, withdrawId)
+{
+//    console.log("mint nft type hash", web3.utils.keccak256(web3.utils.utf8ToHex("mint(address to,address posnft_address,uint256 tokenId,uint256 withdrawId)")))
+    return web3.utils.keccak256(web3.eth.abi.encodeParameters(["bytes32", "address", "address", "uint256", "uint256"], [
+            web3.utils.keccak256(web3.utils.utf8ToHex("mint(address to,address posnft_address,uint256 tokenId,uint256 withdrawId)")),
+            to,
+            posnft_address,
+            tokenId,
+            withdrawId
+        ]));
+}
 
 function claimERC721Hash(erc721_address, to, tokenId, withdrawId)
 {
